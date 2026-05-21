e90f7c642d3de02e873a50835a11de1b228b0621
Mohamed Ali <swimman61@gmail.com>
2026-05-21 12:57:28 +0300

    secure firebase keys

--- app/firebase.js
+++ app/firebase.js
@@ -4,12 +4,12 @@ import { getStorage } from "firebase/storage";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA-D72BLG2CoBsIlfZybuvJucyo3LKcTMg",
-  authDomain: "roadfix-ef1a4.firebaseapp.com",
-  projectId: "roadfix-ef1a4",
-  storageBucket: "roadfix-ef1a4.appspot.com",
-  messagingSenderId: "232775464414",
-  appId: "1:232775464414:web:39e3fa2ee45cd57c87594f"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
